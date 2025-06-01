17c3cd73fcea5b7e701304a81d038af69873d565
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-01 14:22:10 +0900

    Removed redundant auth provider

--- GMFrontend/src/App.tsx
+++ GMFrontend/src/App.tsx
@@ -1,24 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import "./index.css";
-import { Routes, Route, BrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { AuthProvider } from "./contexts/AuthProvider";
-
-import WorkoutDashboard from "./pages/WorkoutDashboard";
 import Register from "./pages/Register";
 import SingleWorkout from "./pages/SingleWorkout";
-
+import WorkoutDashboard from "./pages/WorkoutDashboard";
 import { WORKOUT, WORKOUTS_ENDPOINT } from "./constants/endpoints";
 import { MenuProvider } from "./contexts/MenuProvider";
-import { QueryParamsProvider } from "./contexts/QueryParamProvider";
 import { ModProvider } from "./contexts/ModProvider";
+import { QueryParamsProvider } from "./contexts/QueryParamProvider";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <AuthProvider> */}
         <QueryParamsProvider>
           <ModProvider>
             <MenuProvider>
@@ -38,7 +34,6 @@ function App() {
             </MenuProvider>
           </ModProvider>
         </QueryParamsProvider>
-        {/* </AuthProvider> */}
       </BrowserRouter>
     </>
   );
