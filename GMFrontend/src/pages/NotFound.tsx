18fa9c087a2ffa3255c762d2ac4237566a8e43c9
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-02 20:19:11 +0900

    Cleaned up not found page

--- GMFrontend/src/pages/NotFound.tsx
+++ GMFrontend/src/pages/NotFound.tsx
@@ -8,15 +8,12 @@ const NotFound = () => {
   return (
     <>
       <Nav />
-      <div className="h-[60vh]">
-        <FormContainer>
-          <Title title="404 Not Found" />
-          <p className="text-text text-[1.1rem]">
-            It looks like the resource you were trying to access does not exist.
-          </p>
-        </FormContainer>
-      </div>
-
+      <FormContainer>
+        <Title title="404 Not Found" />
+        <p className="text-text text-[1.1rem]">
+          It looks like the resource you were trying to access does not exist.
+        </p>
+      </FormContainer>
       <Footer />
     </>
   );
