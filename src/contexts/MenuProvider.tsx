808104b5cdcf67b5e58acf6548078465abea3151
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-21 15:43:04 +0900

    more log statements

--- src/contexts/MenuProvider.tsx
+++ src/contexts/MenuProvider.tsx
@@ -19,9 +19,16 @@ export const MenuProvider = ({ children }: Props) => {
       console.log("newId: " + newId);
       console.log("type: " + type);
       console.log("NewType: " + newType);
+      console.log(
+        "Result of conditional: " +
+          (prevId === newId && newType === type ? -1 : newId)
+      );
       return prevId === newId && newType === type ? -1 : newId;
     });
     setType(newType);
+
+    console.log("Final Id: " + openMenuId);
+    console.log("Final type: " + type);
   };
   return (
     <MenuContext.Provider
