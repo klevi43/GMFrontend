55849c22ff04c4d3012d370b105a36281d12a0fe
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-21 15:35:34 +0900

    added console log statements showOpenMenu

--- src/contexts/MenuProvider.tsx
+++ src/contexts/MenuProvider.tsx
@@ -15,6 +15,10 @@ export const MenuProvider = ({ children }: Props) => {
   const [type, setType] = useState<ItemType | undefined>(undefined);
   const showOpenMenu = (newId: number, newType: ItemType) => {
     setOpenMenuId((prevId) => {
+      console.log("PrevId: " + prevId);
+      console.log("newId: " + newId);
+      console.log("type: " + type);
+      console.log("NewType: " + newType);
       return prevId === newId && newType === type ? -1 : newId;
     });
     setType(newType);
