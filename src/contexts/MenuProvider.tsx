4e364dbac8326317be96bf34fab7b2842ad81c06
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-21 15:47:11 +0900

    Bug Fixed Removed log statements

--- src/contexts/MenuProvider.tsx
+++ src/contexts/MenuProvider.tsx
@@ -15,20 +15,9 @@ export const MenuProvider = ({ children }: Props) => {
   const [type, setType] = useState<ItemType | undefined>(undefined);
   const showOpenMenu = (newId: number, newType: ItemType) => {
     setOpenMenuId((prevId) => {
-      console.log("PrevId: " + prevId);
-      console.log("newId: " + newId);
-      console.log("type: " + type);
-      console.log("NewType: " + newType);
-      console.log(
-        "Result of conditional: " +
-          (prevId === newId && newType === type ? -1 : newId)
-      );
       return prevId === newId && newType === type ? -1 : newId;
     });
     setType(newType);
-
-    console.log("Final Id: " + openMenuId);
-    console.log("Final type: " + type);
   };
   return (
     <MenuContext.Provider
