e3f69a659a545832c7b45fc8a2443eadb46e7835
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-07-05 14:59:50 +0900

    changed formContainer width

--- src/components/containers/FormContainer.tsx
+++ src/components/containers/FormContainer.tsx
@@ -5,7 +5,7 @@ interface Props {
 const FormContainer = ({ children }: Props) => {
   return (
     <>
-      <div className="flex flex-col justify-center px-6 py-8 mx-auto mt-10 mb-40 w-full max-w-[50%] md:mb-15 bg-modal border-1 rounded-sm border-modal-outline">
+      <div className="flex flex-col justify-center px-6 py-8 mx-auto mt-10 mb-40 w-full max-w-[400px] md:mb-15 bg-modal border-1 rounded-sm border-modal-outline">
         {children}
       </div>
     </>
