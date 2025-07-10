691e5e368bbeb69c8378ad8fd0ab574242613816
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-07-10 18:28:51 +0900

    Changed some hero styles

--- src/components/hero/HeroText.tsx
+++ src/components/hero/HeroText.tsx
@@ -6,13 +6,12 @@ const HeroText = () => {
     <>
       <div className=" h-auto">
         <FadeIn duration={0.4} delay={0.1} from={75}>
-          <h2 className="text-white text-[4rem] font-bold pl-[1rem]">
-            Track your progress. <br />
-            See results.
+          <h2 className="text-white text-[3.5rem] leading-[1.25] font-bold px-[1rem]">
+            Track your progress. <br /> See results.
           </h2>
         </FadeIn>
         <FadeIn duration={0.4} delay={0.2} from={75}>
-          <h3 className="pl-[1rem] mb-[1rem] text-[1.5rem]">
+          <h3 className="px-[1rem] mb-[1rem] text-[1.5rem]">
             Making the best version of yourself has never been easier.
           </h3>
         </FadeIn>
@@ -20,11 +19,11 @@ const HeroText = () => {
           <div className="flex justify-start pl-[1rem] mb-[1rem] md:mb-0">
             <Link
               to={REGISTER_ENDPOINT}
-              className=" flex items-center group bg-primary py-2 px-4 border-2 border-primary rounded-full hover:pr-[1rem] text-background text-[1.7rem] font-semibold hover:cursor-pointer active:bg-background active:text-primary transition-all duration-200"
+              className=" flex items-center group bg-primary py-1 px-2 border-2 border-primary rounded-full  text-background text-[1.3rem] font-semibold hover:cursor-pointer active:bg-background active:text-primary transition-all duration-200"
             >
               <div>Get Started</div>
               <svg
-                className="mt-[4px] h-[2rem] w-[2rem] fill-background group-hover:ml-[1rem] group-active:ml-[1rem] transition-all duration-150"
+                className="mt-[2px] h-[1.7rem] w-[1.7rem] fill-background group-hover:ml-[1rem] group-active:ml-[1rem] transition-all duration-150"
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
